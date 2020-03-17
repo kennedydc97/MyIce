d6bd78d46fcfac7916c6365edc070495b911791e
Treinamento <Treinamento@RD723911.raiadrogasil.com>
2020-03-17 11:59:59 -0300

    Integração com o banco incompleta

--- MyIce/src/app/components/lista-de-produtos/lista-de-produtos.component.ts
+++ MyIce/src/app/components/lista-de-produtos/lista-de-produtos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Categoria } from 'src/app/models/categorias';
 import { Produto } from 'src/app/models/Produtos';
 import { Router } from '@angular/router';
+import { ProdutosService } from 'src/app/services/produtos.service';
+import { produtoAPI } from 'src/app/models/produtoAPI';
 
 @Component({
   selector: 'app-lista-de-produtos',
@@ -13,18 +15,21 @@ export class ListaDeProdutosComponent implements OnInit {
   produtos: Produto[] = []
   produtosExibidos: Produto[] = []
 
-  constructor(private router: Router) { 
+  produtoAPI: any = [];
+  erro: any;
+
+  constructor(private router: Router, private serviceProduto: ProdutosService) {
+    this.getter(); 
     this.produtos.push(
-      new Produto(1, "assets/produto1.jpg", "Gelo Artificial", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, sint. Reiciendis impedit nam voluptatum nemo similique libero, asperiores laboriosam totam illo ullam earum doloremque. Culpa, quidem! Sequi architecto enim ipsa.", 27, 24, 23, 2),
-      new Produto(2, "assets/gelodecoco.png", "Gelo de Coco", "", 16.65, 15, 10, 3),
-      new Produto(3, "assets/produto3.jpg", "Gelo flexivel", "", 32.50, 30, 27, 2),
-      new Produto(4, "assets/produto4.jpg", "Tech Gel", "", 27, 24, 23, 2),
-      new Produto(5, "assets/produto5.jpg", "Gelo artificial em espuma", "", 24, 12, 10, 2),
-      new Produto(6, "assets/saco-de-gelo-1kg.png", "Saco de Gelo 1kg", "", 13, 10, 8, 1),
-      new Produto(7, "assets/formasdegelo.jpg", "Formas de Gelo", "", 18, 15, 10, 0),
-      new Produto(8, "assets/caixa-de-isopor.png", "Caixa de isopor", "", 50, 24, 23, 0),
-      new Produto(9, "assets/pinguin1.png", "Mascote do site", "", 2000, 1350, 1000, 3),
-      new Produto(10, "assets/Olaf.png", "Olaf", "", 1000, 800, 500, 3),)
+      new Produto(1, "assets/produto1.jpg", "Gelo Artificial", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, sint. Reiciendis impedit nam voluptatum nemo similique libero, asperiores laboriosam totam illo ullam earum doloremque. Culpa, quidem! Sequi architecto enim ipsa.", 27, 24, 2),
+      new Produto(2, "assets/gelodecoco.png", "Gelo de Coco", "", 16.65, 15, 3),
+      new Produto(3, "assets/produto3.jpg", "Gelo flexivel", "", 32.50, 30, 2),
+      new Produto(4, "assets/produto4.jpg", "Tech Gel", "", 27, 24, 2),
+      new Produto(5, "assets/produto5.jpg", "Gelo artificial em espuma", "", 24, 12, 2),
+      new Produto(6, "assets/saco-de-gelo-1kg.png", "Saco de Gelo 1kg", "", 13, 10, 1),
+      new Produto(7, "assets/formasdegelo.jpg", "Formas de Gelo", "", 18, 15, 0),
+      new Produto(8, "assets/caixa-de-isopor.png", "Caixa de isopor", "", 50, 24, 0),
+      new Produto(10, "assets/Olaf.png", "Olaf", "", 1000, 800, 3),)
     for(let i = 0; i < this.produtos.length; i++){
       this.produtosExibidos.push(this.produtos[i])
     }
@@ -64,6 +69,15 @@ export class ListaDeProdutosComponent implements OnInit {
 
   produtoSelecionado(produto){
       this.router.navigate(['/lista-de-produtos', produto.id])
+  }
+
+  getter() {
+    this.serviceProduto.getApi().subscribe(
+      (data: produtoAPI) => {
+        this.produtoAPI = data;
+      }, (error: any) => {
+        console.error("ERROR", error)
+      })
   }
 
 ngOnInit(): void {
---
+++ MyIce/src/app/models/produtoAPI.ts
@@ -0,0 +1,9 @@
+export class produtoAPI{
+    public idProduct?:number; 
+    public name?: string;
+    public description?: string;
+    public image?: string; 
+    public group?: number;
+    public vlProduct?: number;
+    public vlProductFull?: number
+}
