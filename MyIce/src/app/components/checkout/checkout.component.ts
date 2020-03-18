b99b46ee8a4cfa3eb067ac8c3b7ddde4e9932efa
Treinamento <Treinamento@RD723897.raiadrogasil.com>
2020-03-18 17:34:08 -0300

    Algumas validações feitas no pagamento

--- MyIce/src/app/components/checkout/checkout.component.ts
+++ MyIce/src/app/components/checkout/checkout.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 import { StorageService } from 'src/app/services/storage.service'
 import { Carrinho } from 'src/app/models/Carrinho';
-import { Pagamento } from 'src/app/models/Pagamento';
 import { debounceTime } from 'rxjs/operators';
+import { Validar } from 'src/app/models/Validar'
+import { Pagamento } from 'src/app/models/Pagamento';
 
 
 @Component({
@@ -12,14 +13,101 @@ import { debounceTime } from 'rxjs/operators';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-  formPagamento
   carrinho: Carrinho[] = [];
   subTotal: number = 0;
   total: number = 0;
   valoresForm
   conversao
+  validar: Validar;
 
-  // constructor(private cepService: CepService) {
+  formPagamento : FormGroup;
+
+
+  constructor(private storage: StorageService, private fb: FormBuilder) {
+
+    this.carrinho = storage.recuperarCarrinho()
+
+    console.log(storage.recuperarCarrinho());
+
+    this.validar = new Validar();
+  }
+
+    private createForm():FormGroup{
+      return this.fb.group({
+        numero: new FormControl ('',  Validators.compose([
+          Validators.required,
+          Validators.minLength(16),
+          Validators.maxLength(16)
+        ])),
+        vencimento: new FormControl ('', Validators.compose([Validators.required            
+        ])),
+        cvv: new FormControl ('', Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(3)
+        ])),
+        nomeTitular: new FormControl ('',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100)
+        ])),
+        cpf: new FormControl ('', Validators.compose([
+          Validators.required,
+          Validators.maxLength(11),
+          Validators.minLength(11),
+          Validar.ValidarCpf
+        ]))
+        
+      });
+  
+  
+    }
+  
+
+
+  cpfMask = [/[0-9]/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, "-", /\d/, /\d/];
+  numeroCartao = [/[0-9]/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, /\d/]
+  cvv = [/[0-9]/, /\d/, /\d/]
+
+    
+
+
+
+  get nome() {
+    return this.formPagamento.get('nomeTitular');
+  }
+
+  get cpf() {
+    return this.formPagamento.get('cpf');
+  }
+
+  compraRealizada() {
+    this.conversao = JSON.stringify(this.valoresForm);
+    localStorage.setItem('Pagamento', this.conversao);
+  }
+
+  ngOnInit(): void{
+    
+    this.formPagamento = this.createForm();
+
+    console.log(this.valoresForm);
+    this.formPagamento.valueChanges.pipe(
+      debounceTime(1000)).subscribe(res => {
+        console.log(res); 
+        this.valoresForm = res;
+      });
+  }
+}
+
+
+
+
+//   validarPagamento(): boolean {
+//     return this.validar.verificarDadosPagamento(this.formPagamento.value);
+//   }
+// }
+// constructor(private cepService: CepService) {
   //   this.formEntrega = this.createForm(new Entrega());
   //  }
 
@@ -52,17 +140,6 @@ export class CheckoutComponent implements OnInit {
   //   })
   // }
 
-
-
-  constructor(private storage: StorageService, private fb: FormBuilder) {
-
-    this.carrinho = storage.recuperarCarrinho()
-
-    console.log(storage.recuperarCarrinho());
-  }
-
-  data: string;
-
   // private createForm(pagamento: Pagamento): FormGroup {
   //   return new FormGroup({
   //     numero: new FormControl(pagamento.numero),
@@ -72,39 +149,3 @@ export class CheckoutComponent implements OnInit {
   //     cpf: new FormControl(pagamento.cpf)
   //   })
   // }
-
-
-
-  cpfMask = [/[0-9]/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, "-", /\d/, /\d/];
-  numeroCartao = [/[0-9]/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, /\d/]
-  cvv = [/[0-9]/, /\d/, /\d/]
-
-
-
-
-  ngOnInit(): void {
-
-    this.formPagamento = this.fb.group({
-      numero: [''],
-      vencimento: [''],
-      cvv: [''],
-      nomeTitular: [''],
-      cpf: ['']
-
-    });
-    console.log(this.valoresForm);
-    this.formPagamento.valueChanges.pipe(
-      debounceTime(1000)).subscribe(res => {
-        console.log(res); 
-        this.valoresForm = res;
-      });
-  }
-
-
-
-
-  compraRealizada() {
-    this.conversao = JSON.stringify(this.valoresForm);
-    localStorage.setItem('Pagamento', this.conversao);
-  }
-}
--- MyIce/src/app/models/Validar.ts
+++ MyIce/src/app/models/Validar.ts
@@ -1,67 +1,52 @@
 import { Address } from './address';
+import { FormControl} from '@angular/forms';
+import { Pagamento } from 'src/app/models/Pagamento'
 
 
 export class Validar {
-        cancelarLetras(event: any) {
-            let evento = event;
-            let key = evento.keyCode || evento.which;
-            key = String.fromCharCode(key);
-            //let regex = /^[0-9.,]+$/;
-            let regex = /^[0-9]+$/;
-            if (!regex.test(key)) {
-                evento.returnValue = false;
-                if (evento.preventDefault) evento.preventDefault();
-            }
+    static ValidarCpf(controle: FormControl) {
+        const cpf = controle.value;
+
+        let soma: number = 0;
+        let resto: number;
+        let valido: boolean;
+    
+        const regex = new RegExp('[0-9]{11}');
+    
+        if (
+          cpf == '00000000000' ||
+          cpf == '11111111111' ||
+          cpf == '22222222222' ||
+          cpf == '33333333333' ||
+          cpf == '44444444444' ||
+          cpf == '55555555555' ||
+          cpf == '66666666666' ||
+          cpf == '77777777777' ||
+          cpf == '88888888888' ||
+          cpf == '99999999999' ||
+          !regex.test(cpf)
+        )
+          valido = false;
+        else {
+          for (let i = 1; i <= 9; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+          resto = (soma * 10) % 11;
+    
+          if (resto == 10 || resto == 11) resto = 0;
+          if (resto != parseInt(cpf.substring(9, 10))) valido = false;
+    
+          soma = 0;
+          for (let i = 1; i <= 10; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+          resto = (soma * 10) % 11;
+    
+          if (resto == 10 || resto == 11) resto = 0;
+          if (resto != parseInt(cpf.substring(10, 11))) valido = false;
+          valido = true;
         }
     
-        cancelarNumeros(event: any) {
-            let evento = event;
-            let key = evento.keyCode || evento.which;
-            key = String.fromCharCode(key);
-            //let regex = /^[0-9.,]+$/;
-            let regex = /^[a-z A-Z]+$/;
-            if (!regex.test(key)) {
-                evento.returnValue = false;
-                if (evento.preventDefault) evento.preventDefault();
-            }
-        }
+        if (valido) return null;
     
-        verificarEndereco(address: Address) {
-            if (address.bairro.replace(/\ /g, '').length < 10 || address.cep.length != 8 || address.cidade.replace(/\ /g, '').length < 10 ||
-                ( address.numero == undefined) || address.endereco.replace(/\ /g, '').length < 6 ||
-                address.estado == "") {
-                return true;
-            }
-            return false;
-        }
-    
-        verificarDadosPagamento(pagamento) {
-            if (pagamento.numero.length == 16 && (pagamento.validade != null &&
-                pagamento.validade.length != 0) && pagamento.cvv.length == 3 &&
-                pagamento.nomeTitular.replace(/\ /g, '').length > 8 && this.validarCpf(pagamento.cpf)){
-                    return true;
-            }
-            return false;
-        }
-    
-        validarCpf(cpf) {
-            let soma;
-            let resto;
-            soma = 0;
-            if (cpf == "00000000000") return false;
-    
-            for (let i = 1; i <= 9; i++) soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-            resto = (soma * 10) % 11;
-    
-            if ((resto == 10) || (resto == 11)) resto = 0;
-            if (resto != parseInt(cpf.substring(9, 10))) return false;
-    
-            soma = 0;
-            for (var i = 1; i <= 10; i++) soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-            resto = (soma * 10) % 11;
-    
-            if ((resto == 10) || (resto == 11)) resto = 0;
-            if (resto != parseInt(cpf.substring(10, 11))) return false;
-            return true;
-        }
-}
+        return { cpfInvalido: true };
+      }
+    }
