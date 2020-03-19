a3aeab411eb8f5b121d4febfb05377e409cb6288
Treinamento <Treinamento@RD723911.raiadrogasil.com>
2020-03-19 09:42:21 -0300

    adicionar ao carrinho funcionando

--- MyIce/src/app/components/produto-sozinho/produto-sozinho.component.ts
+++ MyIce/src/app/components/produto-sozinho/produto-sozinho.component.ts
@@ -13,37 +13,40 @@ export class ProdutoSozinhoComponent implements OnInit {
 
   public produtoId;
   produtoTela: produtoAPI;
-  produtoLocal=[]
+  produtoLocal: produtoAPI[] = []
 
 
   constructor(private route: ActivatedRoute, private service: ProdutosService) {
     this.produtoId = parseInt(this.route.snapshot.paramMap.get('id'));
     this.service.buscarProdutoId(this.produtoId).subscribe(
-      produto => this.produtoTela = produto 
+      produto => this.produtoTela = produto
     )
   }
 
   ngOnInit(): void {
-    let teste = JSON.parse(localStorage.getItem("produtoCarrinho"))
-    for(let i = 0; i<teste.length; i++){
-      if(teste != null){
-        this.produtoLocal.push(teste[i])
+    let produtosStorage = JSON.parse(localStorage.getItem("produtoCarrinho"))
+    if (produtosStorage != null) {
+      for (let i = 0; i < produtosStorage.length; i++) {
+        if (produtosStorage != null) {
+          this.produtoLocal.push(produtosStorage[i])
+        }
       }
     }
   }
 
-  salvarProduto(){
+  salvarProduto() {
     let count = 0
-    let produtos = JSON.parse(localStorage.getItem("produtoCarrinho"))
-    for(let i = 0; i < this.produtoLocal.length; i++){
-      if(produtos[i] == this.produtoTela){
-        count++
+    let produtos: produtoAPI[] = JSON.parse(localStorage.getItem("produtoCarrinho"))
+    if (produtos != null) {
+      for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].name == this.produtoTela.name)
+          count++
       }
     }
-    if(count == 0){
-    this.produtoLocal.push(this.produtoTela)
-    let produto_json = JSON.stringify(this.produtoLocal)
-    localStorage.setItem("produtoCarrinho", produto_json)
+    if (count == 0) {
+      this.produtoLocal.push(this.produtoTela)
+      let produto_json = JSON.stringify(this.produtoLocal)
+      localStorage.setItem("produtoCarrinho", produto_json)
     }
   }
 
