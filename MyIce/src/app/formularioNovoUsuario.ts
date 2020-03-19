8ca0efe4e31eaf0c3a6d2bcce27936bb2289a2d1
Carlos <carlos.du.kadu@gmail.com>
2020-03-19 14:20:39 -0300

    voltando uma versao do git, muitos erros

--- MyIce/src/app/formularioNovoUsuario.ts
+++ MyIce/src/app/formularioNovoUsuario.ts
@@ -1,16 +1,31 @@
 export class FormularioNovoUsuario {
 
-    constructor(nome: string, email: string, cpf: string, nascimento: Date, senha: string) {
+    constructor(nome: string, email: string, cpf: string, tel: string, nascimento: Date, senha: string, cep: string, endereco: string, complemento: string, bairro: string, cidade: string, estado: string) {
+      
       this.nome = nome;
       this.email = email;
       this.cpf = cpf;
+      this.tel = tel;
       this.nascimento = nascimento;
       this.senha = senha;
+      this.cep = cep;
+      this.endereco = endereco;
+      this.complemento = complemento;
+      this.bairro = bairro;
+      this.cidade = cidade;
+      this.estado = estado;
     }
   
     nome: string;
     email: string;
     cpf: string;
+    tel: string;
     nascimento: Date;
     senha: string;
+    cep: string;
+    endereco: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
   }
--- MyIce/src/app/models/Formulario.ts
+++ MyIce/src/app/models/Formulario.ts
@@ -1,9 +1,9 @@
 export class Formulario {
     constructor(public codFormulario?:number, 
-        public CEPUsuario?: string, 
-        public enderecoUsuario?: string,
-        public numeroEndereco?: number,
-        public complementoEndereco?: string,
+        public cep?: string, 
+        public endereco?: string,
+        public numero?: number,
+        public complemento?: string,
         public bairro?: string,
         public cidade?: string,
         public estado?: string,
