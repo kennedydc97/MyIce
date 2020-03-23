2b906e0f0386cff492a907121cfa0ec596e415b6
Treinamento <Treinamento@RD723897.raiadrogasil.com>
2020-03-23 17:22:54 -0300

    Validações e API Cep funcionando

---
+++ MyIce/src/app/services/cliente.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+
+const URL = "http://localhost:8080/ecommerce/cliente"
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor() { }
+}
